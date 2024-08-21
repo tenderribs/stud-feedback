49b3e5ba0232ef4169da50d77039a2c59768affb
tenderribs <dc3dude@gmail.com>
2024-08-21 17:26:07 +0200

    configure PWA?

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,34 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+      includeAssets: ['favicon.ico', 'apple-touch-icon-180x180.png', 'maskable-icon-512x512.png'],
+      manifest: {
+        name: 'Daily Reports',
+        short_name: 'DailyReports',
+        description: 'Give Students Daily Reports',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      registerType: 'autoUpdate'
+    })
   ],
   resolve: {
     alias: {
